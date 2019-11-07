f97f30e889927d3ed2164bac5c653027c57d5d0f
Aloxaf <bailong104@gmail.com>
2019-11-08 04:11:57 +0800

    [cloudpinyin] fix baidu backend (#9)

--- src/parse.c
+++ src/parse.c
@@ -8,24 +8,6 @@
 #include <ctype.h>
 #include "cloudpinyin.h"
 
-static inline boolean ishex(char ch)
-{
-    if ((ch >= '0' && ch <= '9') || (ch >='a' && ch <='f') || (ch >='A' && ch <='F'))
-        return true;
-    return false;
-}
-
-static inline unsigned char tohex(char ch)
-{
-    if (ch >= '0' && ch <= '9')
-        return ch - '0';
-    if (ch >='a' && ch <='f')
-        return ch - 'a' + 10;
-    if (ch >='A' && ch <='F')
-        return ch - 'A' + 10;
-    return 0;
-}
-
 void SogouParseKey(FcitxCloudPinyin* cloudpinyin, CurlQueue* queue)
 {
     char* str = fcitx_utils_trim(queue->str);
@@ -155,52 +137,15 @@ char* GoogleParsePinyin(FcitxCloudPinyin* cloudpinyin, CurlQueue* queue)
 char* BaiduParsePinyin(FcitxCloudPinyin* cloudpinyin, CurlQueue* queue)
 {
     char *start = NULL, *end = NULL;
-    static iconv_t conv = 0;
-    if (conv == 0)
-        conv = iconv_open("utf-8", "utf-16be");
-
-    if (conv == (iconv_t)(-1))
-        return NULL;
-    if ((start = strstr(queue->str, "[[[\"")) != NULL)
+    if ((start = strstr(queue->str, "[[\"")) != NULL)
     {
-        start += strlen( "[[[\"");
+        start += strlen( "[[\"");
         if ((end = strstr(start, "\",")) != NULL)
         {
             size_t length = end - start;
-            if (length % 6 != 0 || length == 0)
-                return NULL;
-
-            size_t i = 0, j = 0;
-            char* buf = fcitx_utils_malloc0((length / 6 + 1) * 2);
-            while (i < length)
-            {
-                if (start[i] == '\\' && start[i+1] == 'u')
-                {
-                    if (ishex(start[i+2]) && ishex(start[i+3]) && ishex(start[i+4]) && ishex(start[i+5]))
-                    {
-                        buf[j++] = (tohex(start[i+2]) << 4) | tohex(start[i+3]);
-                        buf[j++] = (tohex(start[i+4]) << 4) | tohex(start[i+5]);
-                    }
-                    else
-                        break;
-                }
-
-                i += 6;
-            }
-
-            if (i != length)
-            {
-                free(buf);
-                return NULL;
-            }
-            buf[j++] = 0;
-            buf[j++] = 0;
-            size_t len = UTF8_MAX_LENGTH * (length / 6) * sizeof(char);
-            char* realstring = fcitx_utils_malloc0(UTF8_MAX_LENGTH * (length / 6) * sizeof(char));
-            IconvStr p = buf; char *pp = realstring;
-            iconv(conv, &p, &j, &pp, &len);
-
-            free(buf);
+            char *realstring = fcitx_utils_malloc0(sizeof(char) * (length + 1));
+            strncpy(realstring, start, length);
+            realstring[length] = '\0';
             if (fcitx_utf8_check_string(realstring))
                 return realstring;
             else
